5f4b1276d6e432e7d1320900362cc19977331a9c
Dawson J. White <djw1901@gmail.com>
2023-05-08 18:23:31 -0500

    imported mongoose and created a directory for models, also altered server.js to POST a model called User.js

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 const express = require('express');
-const app = express();
-const port = 3001;
+const mongoose = require('mongoose');
+const User = require('./models/User');
 
-app.get('/', (req, res) => {
-    res.send("Hello World!!!"); // Creates a res or responce from the server to the client
+const port = 3001;
+const app = express();
+app.use(express.json());//This allows parsing JSON request bodies
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/paperTradeDB', {
+    useUrlParser: true,
+    useUnifiedTopology: true
+
+})
+.then(() => console.log('Connected to MongoDB...'))
+.catch(err => console.error('Could not connect to MongoDB...', err));
+
+app.post('/users', async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = new User({
+        username,
+        password
+    });
+
+    try {
+        await user.save();
+        res.send('user saved');
+    }catch(err) {
+        res.status(500),send(err);
+    }
+
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
